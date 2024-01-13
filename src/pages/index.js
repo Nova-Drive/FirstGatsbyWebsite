dd9439bb4e67e4056ece9adffcd76f63981820e3
Cameron <cameron.matthew101@gmail.com>
2024-01-13 00:00:15 -0700

    goobbue

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const IndexPage = () => {
     <main>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Goobbue is love, Goobbue is life.</p>
     </main>
   )
 }
